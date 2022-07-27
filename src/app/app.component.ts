1abd41dbc930b7031ec631c70d0e619d3454123b
Nareg Berberian <naregb94@gmail.com>
2022-07-27 14:08:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Firebase-ToDoList';
+  todos$: Observable<any[]>;
+  constructor(firestore: Firestore) {
+    const coll = collection(firestore, "Todo's");
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos)=> {
+      console.log('neue Todos sind:', newTodos);
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'imarachinfirebaseprojekty',
+    appId: '1:177331463341:web:6cc2013555377cfb03d224',
+    storageBucket: 'imarachinfirebaseprojekty.appspot.com',
+    apiKey: 'AIzaSyBul7TlRLzWucPtF3fQKz5CC-WB25ABKYk',
+    authDomain: 'imarachinfirebaseprojekty.firebaseapp.com',
+    messagingSenderId: '177331463341',
+  },
   production: false
 };
 
